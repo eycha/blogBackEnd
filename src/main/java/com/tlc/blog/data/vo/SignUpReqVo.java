ffd4342b469bea9bbf28b84af3bddb35f4d8fbea
Jung Yeonho <intelzz@naver.com>
2023-05-26 14:56:53 +0900

    로그인, 회원가입 ReqVo Validation 추가2

--- src/main/java/com/tlc/blog/data/vo/SignUpReqVo.java
+++ src/main/java/com/tlc/blog/data/vo/SignUpReqVo.java
@@ -3,9 +3,13 @@ package com.tlc.blog.data.vo;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 public class SignUpReqVo {
+    @NotNull
     private String userId;
+    @NotNull
     private String userPw;
 }
